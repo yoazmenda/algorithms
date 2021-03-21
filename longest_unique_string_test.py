407c84608997f667d9836b5d2906b068bd44e349
yoazmenda <yoazmenda@gmail.com>
2021-03-21 16:58:13 +0200

    optimal solution for longest unique string

--- longest_unique_string_test.py
+++ longest_unique_string_test.py
@@ -4,9 +4,10 @@ import longest_unique_string
 
 class TestLongestUniqueString(unittest.TestCase):
     def test(self):
+        self.assertEqual(longest_unique_string.find("abacdxabyyzx"), 6)
+        self.assertEqual(longest_unique_string.find("12312ab4b"), 6)
         self.assertEqual(longest_unique_string.find("123123"), 3)
         self.assertEqual(longest_unique_string.find("1231234"), 4)
-        self.assertEqual(longest_unique_string.find("12312ab4b"), 6)
         self.assertEqual(longest_unique_string.find(""), 0)
         self.assertEqual(longest_unique_string.find("q"), 1)
         self.assertEqual(longest_unique_string.find("qq"), 1)
