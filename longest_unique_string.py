c623e43ed62bf0d6433a89655010ae86615d5a03
yoazmenda <yoazmenda@gmail.com>
2021-03-21 16:59:07 +0200

    optimal solution for longest unique string

--- longest_unique_string.py
+++ longest_unique_string.py
@@ -1,7 +1,34 @@
 def find(string):
-    return naive(string)
+    return optimized(string)
 
 
+# O(n)
+def optimized(string):
+    longest_start = 0
+    longest_end = 0
+    current_start = 0
+    current_end = 0
+    dic = {}
+    for i in range(len(string)):
+        c = string[i]
+        if c in dic:
+            prev = dic[c]
+            if prev >= current_start:
+                if current_end - current_start > longest_end - longest_start:
+                    longest_start = current_start
+                    longest_end = current_end
+                current_start = prev + 1
+        dic[c] = i
+        current_end += 1
+    if current_end - current_start > longest_end - longest_start:
+        longest_start = current_start
+        longest_end = current_end
+    winner = string[longest_start:longest_end]
+    print("Longest unique substring: {}".format(winner))
+    return len(winner)
+
+
+# O(n^3)
 def naive(string):
     max = 0
     for i in range(len(string)):
